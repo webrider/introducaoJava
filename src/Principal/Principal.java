54f75b4220cbb17fea1f051a41b06aca6b2b1164
Rider Rocha Cantuaria <4382064@univali.br>
2016-08-23 12:10:24 -0300

    Envio Inicial de Arquivos

---
+++ src/Principal/Principal.java
@@ -0,0 +1,21 @@
+package Principal;
+
+/**
+ * Classe Principal do sistema
+ *
+ * @author Ríder Cantuária <cantuaria@edu.univali.br>
+ * @version beta
+ * @since 08/2016
+ */
+public class Principal {
+
+    /**
+     * Main Java 
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Primeiro Código");
+    }
+
+}
